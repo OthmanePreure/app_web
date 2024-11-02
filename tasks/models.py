c6eabf9ab2e3c16510d35049d0f37c6e061441c8
preure othmane <preureoth@gmail.com>
2024-11-02 16:22:37 +0100

    delete unique

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Collection(models.Model):
     name = models.CharField(max_length=60)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField()
 
 
 class Task(models.Model):
